3dd92e4a30f63759b651e3817ed2ca8b89ddd0a9
Colin <1829240+yecllsl@users.noreply.github.com>
2024-05-06 10:34:16 +0800

    修改默认代码，使能正常运行

--- Chapter 03/draw3d.py
+++ Chapter 03/draw3d.py
@@ -7,18 +7,27 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D, proj3d
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from colors import *
+import numpy as np
 
 ## https://stackoverflow.com/a/22867877/1704140
 class FancyArrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
-        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        #FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        super().__init__((0,0), (0,0), *args, **kwargs)
         self._verts3d = xs, ys, zs
 
     def draw(self, renderer):
         xs3d, ys3d, zs3d = self._verts3d
-        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        #xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
         self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
         FancyArrowPatch.draw(self, renderer)
+        
+    def do_3d_projection(self, renderer=None):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        return np.min(zs)
 
 
 class Polygon3D():
@@ -124,7 +133,10 @@ def draw3d(*objects, origin=True, axes=True, width=6, save_as=None, azim=None, e
 
         elif type(object) == Arrow3D:
             xs, ys, zs = zip(object.tail, object.tip)
-            a = FancyArrow3D(xs,ys,zs, mutation_scale=20,arrowstyle='-|>', color=object.color)
+            # a = FancyArrow3D(xs,ys,zs, mutation_scale=20,arrowstyle='-|>', color=object.color)
+            # ax.add_artist(a)
+            arrow_prop_dict = dict(mutation_scale=20, arrowstyle='-|>', color='k', shrinkA=0, shrinkB=0)
+            a = FancyArrow3D(xs, ys, zs, **arrow_prop_dict)
             ax.add_artist(a)
 
         elif type(object) == Segment3D:
